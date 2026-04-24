0aadb03cc16be4807049602f5f04dbd981fc8141
Basmala Wael <basmalawael86@gmail.com>
2026-04-25 00:47:21 +0700

    Badge

--- G-NET-12-EF02/Model/Badge.cs
+++ G-NET-12-EF02/Model/Badge.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace G_NET_12_EF02.Model
 {
-    internal class Badge
+    internal class Badge 
     {
         public int BadgeID { get; set; }
         public string BadgeNumber { get; set; }
